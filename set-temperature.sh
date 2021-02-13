5b4e0c11904480c4492711f4f733136d7204cb0f
koliveira92 <kelvin.oliveira92@outlook.com>
2021-02-13 17:20:08 -0300

    chore(*): Create kafka image

---
+++ set-temperature.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker exec -it kafka_node_js_broker_1 /opt/kafka/bin/kafka-console-producer.sh --bootstrap-server localhost:9092 --topic temperature
---
+++ start-kafka.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker-compose -p kafka_node_js up -d
